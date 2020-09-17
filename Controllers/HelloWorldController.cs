e1d95c02bdb707c1546899054734c10a94dbdc06
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-09-17 12:32:16 +0500

    Controller + Model setup

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -1,21 +1,22 @@
-using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
 
-namespace MvcMovie.Controllers
-{
-    public class HelloWorldController: Controller
-    {
+namespace MvcMovie.Controllers {
+    public class HelloWorldController : Controller {
 
         // 
         // GET: /HelloWorld/
-        public IActionResult Index(){
-            return View();
+        public IActionResult Index () {
+            return View ();
         }
 
         // 
         // GET: /HelloWorld/Welcome/
-        public string Welcome(string name, int numTimes = 1){
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is {numTimes}");
+        public IActionResult Welcome (string name, int numTimes = 1) {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View ();
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,65 +1,58 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MvcMovie.Data;
 
-namespace MvcMovie
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace MvcMovie {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddControllersWithViews ();
+            services.AddDbContext<MvcMovieContext> (options =>
+                options.UseSqlite (Configuration.GetConnectionString ("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            } else {
+                app.UseExceptionHandler ("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseHsts ();
             }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            app.UseHttpsRedirection ();
+            app.UseStaticFiles ();
 
-            app.UseRouting();
+            app.UseRouting ();
 
-            app.UseAuthorization();
+            app.UseAuthorization ();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllerRoute (
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
 
-            #if DEBUG
-            try
-            {
-                File.WriteAllText("browsersync-update.txt", DateTime.Now.ToString());
-            }
-            catch { 
+#if DEBUG
+            try {
+                File.WriteAllText ("browsersync-update.txt", DateTime.Now.ToString ());
+            } catch {
                 // ignore
             }
 #endif
